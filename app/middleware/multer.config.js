0775be000aaa1ef2d94d3470950151d98ae02f36
DiegoBulhoes <diegobulhoesm@gmail.com>
2020-05-30 01:09:15 -0400

    Fix  Eslint

--- app/middleware/multer.config.js
+++ app/middleware/multer.config.js
@@ -8,7 +8,10 @@ const storage = multer.diskStorage({
     callback(null, DIR);
   },
   filename(req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
+    );
   },
 });
 
